9011f416e4b1d3291c14766bd742262548d995ca
Wayne Wollesen <wayniackc@gmail.com>
2021-05-09 11:37:23 -0500

    maybe it's working

--- app.py
+++ app.py
@@ -10,13 +10,49 @@ app = Flask(__name__)
 
 def GetWeather(zipcode):
     url = f"https://api.openweathermap.org/data/2.5/weather?zip={zipcode}&appid={keys.weather_api_key}"
-    print(url)
+    #print(url)
 
     response = requests.get(url)
-    print(response.json())
+    #print(response.json())
 
     temp = round(1.8 * (response.json()['main']['temp'] - 273) + 32, 1)
-    print(temp)
+    #print(temp)
+    desc = response.json()['weather'][0]['description']
+    #print(desc)
+    weather_icon = response.json()['weather'][0]['icon']
+    #print(weather_icon)
+    icon_url = f"http://openweathermap.org/img/wn/{weather_icon}.png"
+    #print(icon_url)
+
+    dic = {
+        "response_type": "ephemeral",
+        "text": f"The sky is showing {desc} with a current temperature of {temp}F.",
+        "username": "AskWayne",
+        "icon_url": icon_url
+    }
+    #print(dic)
+
+    json_body = json.dumps(dic)
+    #print(json_body)
+
+    #print(len(json_body))
+    
+    mm_response = build_response(json_body)
+    print(mm_response)
+    return mm_response
+
+def build_response(json_body):
+    length = len(json_body)
+
+    response = f"""
+    HTTP/1.1 200 OK
+    Content-Type: application/json
+    Content-Length: {length}
+
+    {json_body}
+    """
+    #print(response)
+    return response
 
 
 @app.route('/askwayne', methods = ['POST'])
@@ -28,11 +64,11 @@ def slash_command():
     print(arguments[1])
     if arguments[0].lower() == "weather":
         zip = arguments[1]
-        GetWeather(zip)
-    return 'text'
-
+        response = GetWeather(zip)
+        return response
+        #return GetWeather(zip)
+    #return 'text'
+    #return response
 
 
 app.run(host='0.0.0.0', port=5005)
-
-# api key 1a4f49e7b89b318dd4588b95707876ef
