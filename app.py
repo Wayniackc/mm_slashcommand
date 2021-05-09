69b5a0ec130bf30117e5c7e1b6740f108d9e62fc
Wayne Wollesen <wayniackc@gmail.com>
2021-05-09 15:24:37 -0500

    added mm key check

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 import requests
 import json
 
-from flask import Flask, Response, request
+from flask import Flask, Response, request, abort
 
 import keys
 
 app = Flask(__name__)
 
+
+@app.route('/askwayne', methods = ['POST'])
+def slash_command():
+    # Make sure request comes from Mattermost
+    if keys.mm_token != request.form.getlist('token')[0]:
+        abort(403)
+
+    text = request.form.getlist('text')
+    arguments = text[0].split(' ')
+    if arguments[0].lower() == "weather":
+        zip = arguments[1]
+        mm_response = get_weather(zip)
+        return mm_response
+
+
 def get_weather(zipcode):
     url = f"https://api.openweathermap.org/data/2.5/weather?zip={zipcode}&appid={keys.weather_api_key}"
-    #print(url)
 
     response = requests.get(url)
-    #print(response.json())
 
     temp = round(1.8 * (response.json()['main']['temp'] - 273) + 32, 1)
-    #print(temp)
     desc = response.json()['weather'][0]['description']
-    #print(desc)
     weather_icon = response.json()['weather'][0]['icon']
-    #print(weather_icon)
     icon_url = f"http://openweathermap.org/img/wn/{weather_icon}.png"
-    #print(icon_url)
 
     dic = {
         "response_type": "ephemeral",
@@ -29,53 +38,13 @@ def get_weather(zipcode):
         "username": "AskWayne",
         "icon_url": icon_url
     }
-    #print(dic)
 
     json_body = json.dumps(dic)
-    #print(json_body)
-
-    #print(len(json_body))
-    
-    # mm_response = build_response(json_body)
-    # print(mm_response)
-    # return mm_response
 
     mm_response = Response(response=json_body,
                     status=200,
                     mimetype="application/json")
     return mm_response
-
-
-def build_response(json_body):
-    length = len(json_body)
-
-    response = f"""
-    HTTP/1.1 200 OK
-    Content-Type: application/json
-    Content-Length: {length}
-
-    {json_body}
-    """
-    #print(response)
-    return response
-
-
-@app.route('/askwayne', methods = ['POST'])
-def slash_command():
-    # response = flask.Response()
-    # response.headers["Content-Type"] = "application/json"
-    text = request.form.getlist('text')
-    print(text)
-    arguments = text[0].split(' ')
-    print(arguments[0])
-    print(arguments[1])
-    if arguments[0].lower() == "weather":
-        zip = arguments[1]
-        mm_response = get_weather(zip)
-        return mm_response
-        #return GetWeather(zip)
-    #return 'text'
-    #return response
 
 
 app.run(host='0.0.0.0', port=5005)
